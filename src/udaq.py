31fa3978cb8c7805f6846d616337ddd414293aff
rileyle <lriley@ursinus.edu>
2022-07-19 12:12:10 -0400

    Added branch 'traces' to develop the option to write traces to a RunXXXX_traces.csv file.

--- src/udaq.py
+++ src/udaq.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import configparser
 import csv
 from math import floor
+from itertools import groupby
 
 from src.picoscope_5000a import PicoScope5000A, INPUT_RANGES
 
@@ -15,6 +16,7 @@ class udaq():
         self._CHANNELS = ['A', 'B', 'C', 'D']
 
         self._num_events = 0
+        self._num_traces = 0
 
         self._coupling = {'A': 'DC', 'B': 'DC', 'C': 'DC', 'D': 'DC'}
         self._polarity = {'A': 1, 'B': 1, 'C': 1, 'D': 1}
@@ -30,6 +32,8 @@ class udaq():
                                                 'C': False, 'D': False}
         self._threshold = {'A': 0.0, 'B': 0.0, 'C': 0.0, 'D': 0.0}
         self._timing = {'A': 'PEAK', 'B': 'PEAK', 'C': 'PEAK', 'D': 'PEAK'}
+        self._traces = {'A': 0, 'B': 0, 'C': 0, 'D': 0}
+        self._pulse_width = {'A': 10, 'B': 10, 'C': 10, 'D': 10}
 
         self._timebase = 0
         self._sample_interval = 0
@@ -49,54 +53,64 @@ class udaq():
         self._run_number = 0
         self._output_filename = None
         self._output_file = None
+        self._trace_filename = None
+        self._trace_file = None
+        self._trace_file_isopen = False
 
         if len(sys.argv) == 2:
             self._config_filename = Path.cwd()/str(sys.argv[1])
         else:
             self._config_filename = Path.cwd()/'udaq.config'
-
+        
+        self._config = None
         self._read_configuration()
 
         self.scope = PicoScope5000A()
 
     def _read_configuration(self):
         print(f'Configuration file: {self._config_filename}')
-        config = configparser.ConfigParser(allow_no_value=True)
-        config.read(str(self._config_filename))
+        self._config = configparser.ConfigParser(allow_no_value=True)
+        self._config.read(str(self._config_filename))
 
-        path = config['Run']['Output Path']
+        path = self._config['Run']['Output Path']
         if path == '.':
             self._output_path = Path.cwd()
         else:
             self._output_path = Path.cwd()/path
-        self._run_time = config.getfloat('Run', 'Run Time')
-        if config.has_option('Run','Number of Runs'):
-            self._num_runs = config.getint('Run', 'Number of Runs')
+        self._run_time = self._config.getfloat('Run', 'Run Time')
+        if self._config.has_option('Run','Number of Runs'):
+            self._num_runs = self._config.getint('Run', 'Number of Runs')
 
-        self._pre_trigger_window = config.getfloat('Sampling',
-                                                    'Pre-Trigger Window')
-        self._post_trigger_window = config.getfloat('Sampling',
-                                                    'Post-Trigger Window')
-        self._timebase = config.getint('Sampling', 'Time Base')
-        self._num_captures = config.getint('Sampling',
-                                           'Captures Per Block')
+        self._pre_trigger_window = self._config.getfloat('Sampling',
+                                                         'Pre-Trigger Window')
+        self._post_trigger_window \
+            = self._config.getfloat('Sampling','Post-Trigger Window')
+        self._timebase = self._config.getint('Sampling', 'Time Base')
+        self._num_captures = self._config.getint('Sampling',
+                                                 'Captures Per Block')
 
         for ch in self._CHANNELS:
             sec = 'Channel ' + ch
-            self._is_enabled[ch] = sec in config.sections()
+            self._is_enabled[ch] = sec in self._config.sections()
             if self._is_enabled[ch]:
-                self._coupling[ch] = config[sec]['Coupling']
-                self._polarity[ch] = np.sign(config.getint(sec, 'Polarity'))
-                self._range[ch] = config.getfloat(sec, 'Range')
+                self._coupling[ch] = self._config[sec]['Coupling']
+                self._polarity[ch] \
+                    = np.sign(self._config.getint(sec, 'Polarity'))
+                self._range[ch] = self._config.getfloat(sec, 'Range')
                 self._is_baseline_correction_enabled[ch] \
-                    = config[sec].getboolean('Baseline Correction')
-                self._timing[ch] = config[sec]['Timing']
+                    = self._config[sec].getboolean('Baseline Correction')
+                self._timing[ch] = self._config[sec]['Timing']
                 self._is_trigger_enabled[ch] \
-                    = config[sec].getboolean('Trigger Enabled')
-                self._trigger_type[ch] = config[sec]['Trigger Type']
+                    = self._config[sec].getboolean('Trigger Enabled')
+                self._trigger_type[ch] = self._config[sec]['Trigger Type']
                 self._trigger_direction[ch] \
-                    = config[sec]['Trigger Direction']
-                self._threshold[ch] = config[sec].getfloat('Threshold')
+                    = self._config[sec]['Trigger Direction']
+                self._threshold[ch] = self._config[sec].getfloat('Threshold')
+                if self._config.has_option(sec,'Traces'):
+                    self._traces[ch] = self._config[sec].getint('Traces')
+                if self._config.has_option(sec,'Pulse Width'):
+                    self._pulse_width[ch] \
+                        = self._config[sec].getint('Pulse Width')
 
     def _set_channels(self):
         for ch in self._CHANNELS:
@@ -117,10 +131,6 @@ class udaq():
             self._trigger_type, self._trigger_direction, self._threshold, )
 
     def _open_output_file(self):
-        for x in Path(self._output_path).glob('*.csv'):
-            if x.is_file() and x.name[0:3] == 'Run':
-                self._run_number = int(x.name[3:7]) + 1
-
         self._output_filename =  self._output_path / 'Run{0:04d}.csv'\
                                                        .format(self._run_number)
 
@@ -140,6 +150,18 @@ class udaq():
                 header.append('pulse_height_'+ch)
         writer.writerow(header)
 
+    def _open_trace_file(self):
+        self._trace_filename = self._output_path / 'Run{0:04d}_traces.csv'\
+                                                       .format(self._run_number)
+        try:
+            self._trace_file = open(self._trace_filename, 'w',
+                                    newline='')
+        except IOError:
+            print('Error: Unable to open: {}'\
+                  .format(self._trace_filename))
+            return 0
+        self._trace_file_isopen = True
+    
     def _process_data(self, data):
         x = data['x']
         times, baselines, pulseheights = [], [], []
@@ -181,16 +203,60 @@ class udaq():
                     bl = np.zeros(len(channel_data))
                 ph = (channel_data.max(axis=1) - bl)*1e3
                 pulseheights.append(ph)
-        times = np.array(times)
+        #times = np.array(times) # This is redundant.
         return np.array(times), np.array(pulseheights)
+
+    def _process_traces(self, data):
+        x = data['x']
+        for ch in self._CHANNELS:
+            if self._is_enabled[ch] and self._traces[ch] > 0:
+                channel_data = data[ch]
+                for trace in channel_data:
+                    grad = np.gradient(trace)
+                    # Find groups of consecutive identical values in an array.
+                    # https://stackoverflow.com/questions/6352425
+                    grouped_grad = [(k, sum(1 for i in g)) for k,g in groupby(grad>0)]
+
+                    # Identify pulses within grouped_gradient list.
+                    signals = [(gg[0] == True and gg[1] > self._pulse_width[ch]) for gg in grouped_grad]
+                    number_of_signals = sum(signals)
+
+                    if number_of_signals >= self._traces[ch]:
+                        # Indices of items in the trace at the beginning of each group
+                        # in grouped_gradient list.
+                        items = [gg[1] for gg in grouped_grad]
+                        indices = np.cumsum(items)
+
+                        # Look up the indices and widths of the pulses in the trace.
+                        iSignals = np.where(signals)[0] - 1
+                        i1 = indices[iSignals[0]]
+                        w1 = items[iSignals[0]+1]
+                        
+                        if number_of_signals > 1:
+                            i2 = indices[iSignals[1]]
+                            w2 = items[iSignals[1]+1]
+
+                        if (self._traces[ch] == 1) or \
+                            ((self._traces[ch] == 2) and (i2 - i1 > w1 + w2)):
+                            self._write_trace(ch, x, trace)
+                            self._num_traces += 1
+
+    def _write_trace(self, ch, t, trace):
+        if not self._trace_file_isopen:
+            self._open_trace_file()
+        self._trace_file.write(ch+'\n')
+        # Use better precision than 12 bits to be safe.
+        np.savetxt(self._trace_file, [t], delimiter=',', fmt='%.5e')
+        np.savetxt(self._trace_file, [trace], delimiter=',', fmt='%.5e')
 
     def _write_output(self, times, pulseheights):
         writer = csv.writer(self._output_file, quoting=csv.QUOTE_NONE)
         table = []
+        # There are data for each enabled channel.
         for i in range(len(times)):
-            if self._is_enabled[self._CHANNELS[i]]:
-                table.append(times[i])
-                table.append(pulseheights[i])
+            table.append(times[i])
+            table.append(pulseheights[i])
+        # Organize into rows by event
         for row in zip(*table):
             writer.writerow(row)
 
@@ -200,6 +266,14 @@ class udaq():
         except IOError:
             print('Error: Unable to close: {}'\
                   .format(self._output_filename))
+
+    def _close_trace_file(self):
+        try:
+            self._trace_file.close()
+        except IOError:
+            print('Error: Unable to close: {}'\
+                  .format(self._trace_filename))
+        self._trace_file_isopen = False
 
     def _write_info_file(self):
         info_filename = self._output_filename.with_suffix('.info')
@@ -212,6 +286,7 @@ class udaq():
         info_file.write(f'Start Time: {time.ctime(self._t_start_run)}\n')
         info_file.write(f'Run Time (s): {self._elapsed_time:.1f}\n')
         info_file.write(f'Events: {self._num_events}\n')
+        info_file.write(f'Traces: {self._num_traces}\n')
 
         info_file.write('\n[Sampling]\n')
         info_file.write('Pre-Trigger Window (s): {0:.2e}\n'\
@@ -240,11 +315,21 @@ class udaq():
                     .format(self._trigger_direction[ch]))
                 info_file.write('Threshold (V): {0:.4f}\n'\
                     .format(self._threshold[ch]))
+                sec = 'Channel ' + ch
+                if self._config.has_option(sec,'Traces'):
+                    info_file.write('Traces: {0:d}\n'\
+                    .format(self._traces[ch]))
+                if self._config.has_option(sec,'Pulse Width'):
+                    info_file.write('Pulse Width: {0:d}\n'\
+                    .format(self._pulse_width[ch]))
 
         info_file.close()
 
     def _acquire_run(self):
 
+        for x in Path(self._output_path).glob('*.csv'):
+            if x.is_file() and x.name[0:3] == 'Run':
+                self._run_number = int(x.name[3:7]) + 1
         self._open_output_file()
 
         print(f'\nRun{self._run_number:04d}')
@@ -252,6 +337,7 @@ class udaq():
         self._t_start_run = time.time()
         self._elapsed_time = 0
         self._num_events = 0
+        self._num_traces = 0
         while self._elapsed_time < self._run_time:
             self.scope.start_run(self._pre_samples, self._post_samples,
                                  self._timebase, self._num_captures)
@@ -265,15 +351,20 @@ class udaq():
             times, pulseheights = self._process_data(data)
 
             self._write_output(times, pulseheights)
+            
+            self._process_traces(data)
 
             self._elapsed_time = time.time() - self._t_start_run
 
-            print('Elapsed time: {0:.1f} s / {1:0.1f} s  | {2} events\r'\
-                .format(self._elapsed_time, self._run_time, self._num_events),
+            print('Elapsed time: {0:.1f} s / {1:0.1f} s  | {2} events | {3} traces\r'\
+                .format(self._elapsed_time, self._run_time, self._num_events, self._num_traces),
                 end = '')
 
         self._close_output_file()
 
+        if self._trace_file_isopen:
+            self._close_trace_file()
+            
         self._write_info_file()
 
     def acquire(self):
